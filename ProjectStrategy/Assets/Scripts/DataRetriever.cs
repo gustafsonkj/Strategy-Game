60ded9f825076fdaf872c7be3b19e4c1b549dfa5
Logan Douglass <lcdouglass5352@pointloma.edu>
2016-12-08 14:29:37 -0800

    Making changes to the Data Retriever class - this class will contain all the data about the level a player is trying to save.

--- ProjectStrategy/Assets/Scripts/DataRetriever.cs
+++ ProjectStrategy/Assets/Scripts/DataRetriever.cs
@@ -6,8 +6,12 @@ using System.Runtime.Serialization.Formatters.Binary;
 public class DataRetriever : MonoBehaviour {
     public string currentLevel;//get current level the player is on
     List<Building> buildings = new List<Building>();//get teams and hitpoints
-    public Game game;//get current day, current team
-    List<Unit> units = new List<Unit>();//get locations, teams, colors, types, and health/strength
+    public static Game game;//get current day, current team
+    //get the unit lists from the team script
+    List<Unit> unitsA = game.Teams[0].Units;//team 1 units
+    List<Unit> unitsB = game.Teams[1].Units;//team 2 units
+    List<Building> buildingsA = game.Teams[0].Buildings;//team 1 buildings
+    List<Building> buildingB = game.Teams[1].Buildings;//team 2 buildings
     public Team team;//get resources for each team
     // Use this for initialization
     void Start ()
