f3de797be02d57852fb0ff5f3b2834a03677f617
gustafsonkj <kaigus96@gmail.com>
2016-12-10 10:53:31 -0800

    strategy camera works better now

--- ProjectStrategy/Assets/Scripts/StrategyCamera.cs
+++ ProjectStrategy/Assets/Scripts/StrategyCamera.cs
@@ -21,23 +21,22 @@ public class StrategyCamera : MonoBehaviour {
             return;
 
         // Left / Right
-        //if (transform.position.x < 15 && transform.position.x > -12 && transform.position.z < 35 && transform.position.z > -4)
-        //{
-            if (Input.mousePosition.x < scrollDistance && transform.position.x > -11)
-                transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime);
-            else if (Input.mousePosition.x >= Screen.width - scrollDistance && transform.position.x < 15)
-                transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
+
+        if (Input.mousePosition.x < scrollDistance && transform.position.x > -2)
+            transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime);
+        else if (Input.mousePosition.x >= Screen.width - scrollDistance && transform.position.x < 10)
+            transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
 
         // Forward / Backward
-            if (Input.mousePosition.y < scrollDistance && transform.position.z < 32)
-                transform.Translate((Vector3.forward - transform.forward) * -scrollSpeed * Time.deltaTime);
-            else if (Input.mousePosition.y >= Screen.height - scrollDistance && transform.position.z > -4)
-                transform.Translate((Vector3.forward - transform.forward) * scrollSpeed * Time.deltaTime);
-        //}
-        // Zooming
-        if (Input.GetAxis("Mouse ScrollWheel") < 0)
-            transform.Translate((Vector3.forward + transform.forward) * -scrollSpeed * Time.deltaTime);
-        else if (Input.GetAxis("Mouse ScrollWheel") > 0)
-            transform.Translate((Vector3.forward + transform.forward) * scrollSpeed * Time.deltaTime);
+        if (Input.mousePosition.y < scrollDistance && transform.position.z > 0)
+            transform.Translate((Vector3.forward - transform.forward) * -scrollSpeed * Time.deltaTime);
+        else if (Input.mousePosition.y >= Screen.height - scrollDistance && transform.position.z < 28)
+            transform.Translate((Vector3.forward - transform.forward) * scrollSpeed * Time.deltaTime);
+
+        //// Zooming
+        //if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        //    transform.Translate((Vector3.forward + transform.forward) * -scrollSpeed * Time.deltaTime);
+        //else if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        //    transform.Translate((Vector3.forward + transform.forward) * scrollSpeed * Time.deltaTime);
     }
 }
