2c3c254633019149397fb9218a6813159d4ee050
Logan Douglass <lcdouglass5352@pointloma.edu>
2016-12-09 22:43:42 -0800

    no message

--- ProjectStrategy/Assets/Scripts/DataRetriever.cs
+++ ProjectStrategy/Assets/Scripts/DataRetriever.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 
 
 public class DataRetriever : MonoBehaviour {
-    public static Scene currentLevel;//get current level the player is on
+    public static Scene currentLevel1;//get current level the player is on
     protected static Game Game;//get current day, current team
     //get the unit lists from the team script
     public static List<Unit> unitsA;//team 1 units
@@ -17,13 +17,12 @@ public class DataRetriever : MonoBehaviour {
     public static int teamARes;
     public static int teamBRes;
     public static int Day;
-    public static Team currentTeam;
-
-    // Use this for initialization
-    void Start ()
-    {
-        //saveAllData();
-	}
+    public static int currentTeam;
+    public static int currentLevel;
+    public static List<Vector3> unitPositionsTeam1;
+    public static List<Vector3> unitPositionsTeam2;
+    public static List<int> unitTypesTeam1;
+    public static List<int> unitTypesTeam2;
 
     public static void saveAllData()
     {
@@ -37,48 +36,51 @@ public class DataRetriever : MonoBehaviour {
         teamARes = Game.Teams[0].Resources;
         teamBRes = Game.Teams[1].Resources;
         Day = Game.Day;
-        currentTeam = Game.GetCurrentTeam();
-        currentLevel = SceneManager.GetActiveScene();
+        currentTeam = Game.CurrentTeam;
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+        foreach(Unit u in unitsA)
+        {
+            unitPositionsTeam1.Add(u.transform.position);
+        }
+        foreach (Unit u in unitsB)
+        {
+            unitPositionsTeam2.Add(u.transform.position);
+        }
+        foreach (Unit u in unitsA)
+        {
+            unitTypesTeam1.Add(u.Type);
+        }
+        foreach (Unit u in unitsB)
+        {
+            unitTypesTeam2.Add(u.Type);
+        }
     }
 }
 
 [System.Serializable]
 public class AllMyData
 {
-    private Scene currentLevel;
-    private List<Unit> unitsTeam1;
-    private List<Unit> unitsTeam2;
-    private List<Building> buildingsTeam1;
-    private List<Building> buildingsTeam2;
-    private int team1Resources;
-    private int team2Resources;
-    private int currentDay;
-    private Team currentTeam;
-
-    public void saveDatStuff()
-    {
-        currentLevel = DataRetriever.currentLevel;
-        currentTeam = DataRetriever.currentTeam;
-        currentDay = DataRetriever.Day;
-        team1Resources = DataRetriever.teamARes;
-        team2Resources = DataRetriever.teamBRes;
-        unitsTeam1 = DataRetriever.unitsA;
-        unitsTeam2 = DataRetriever.unitsB;
-        buildingsTeam1 = DataRetriever.buildingsA;
-        buildingsTeam2 = DataRetriever.buildingsB;
-    }
+    public int currentLevel = DataRetriever.currentLevel;//loadscene
+    public int currentTeam = DataRetriever.currentTeam;//game.currentteam?
+    public int currentDay = DataRetriever.Day;//game.day
+    public int team1Resources = DataRetriever.teamARes;//game.teams[0].resources
+    public int team2Resources = DataRetriever.teamBRes;//game.teams[1].resources
+    public List<Unit> unitsTeam1 = DataRetriever.unitsA;//game.teams[0].units
+    public List<Unit> unitsTeam2 = DataRetriever.unitsB;//game.teams[1].units
+    public List<Building> buildingsTeam1 = DataRetriever.buildingsA;//game.teams[0].buildings
+    public List<Building> buildingsTeam2 = DataRetriever.buildingsB;//game.teams[1].buildings
 }
 
 public class saver : MonoBehaviour
 {
     public static void saveGame()
     {
-        if (File.Exists(Application.persistentDataPath + "strategygamesave"))
+        if (File.Exists(Application.persistentDataPath + "/strategygame.save"))
         {
             BinaryFormatter bf = new BinaryFormatter();
             AllMyData amd = new AllMyData();
-            amd.saveDatStuff();
-            FileStream fs = File.Open(Application.persistentDataPath + "strategygamesave", FileMode.Open);
+            File.Delete(Application.persistentDataPath + "/strategygame.save");
+            FileStream fs = File.Create(Application.persistentDataPath + "/strategygame.save");
             bf.Serialize(fs, amd);
             fs.Close();
         }
@@ -86,8 +88,7 @@ public class saver : MonoBehaviour
         {
             BinaryFormatter bf = new BinaryFormatter();
             AllMyData amd = new AllMyData();
-            amd.saveDatStuff();
-            FileStream fs = File.Create(Application.persistentDataPath + "strategygamesave");
+            FileStream fs = File.Create(Application.persistentDataPath + "/strategygame.save");
             bf.Serialize(fs, amd);
             fs.Close();
         }
@@ -95,6 +96,13 @@ public class saver : MonoBehaviour
 
     public static void loadGame()
     {
+        if (File.Exists(Application.persistentDataPath+"/strategygame.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = File.Open(Application.persistentDataPath + "/strategygame.save", FileMode.Open);
+            AllMyData amd = (AllMyData)bf.Deserialize(fs);
+            SceneManager.LoadScene(amd.currentLevel);
 
+        }
     }
 }
---
+++ ProjectStrategy/Assets/newbutton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class newbutton : MonoBehaviour {
+
+	void OnMouseDown()
+    {
+        saver.loadGame();
+    }
+}
