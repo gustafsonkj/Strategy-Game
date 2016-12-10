44068b440fc7c683648e4b0ce3e3f362eb7a9f9c
gustafsonkj <kaigus96@gmail.com>
2016-12-09 23:12:30 -0800

    no message

--- ProjectStrategy/Assets/Scripts/StrategyCamera.cs
+++ ProjectStrategy/Assets/Scripts/StrategyCamera.cs
@@ -21,17 +21,19 @@ public class StrategyCamera : MonoBehaviour {
             return;
 
         // Left / Right
-        if (Input.mousePosition.x < scrollDistance)
-            transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime);
-        else if (Input.mousePosition.x >= Screen.width - scrollDistance)
-            transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
+        //if (transform.position.x < 15 && transform.position.x > -12 && transform.position.z < 35 && transform.position.z > -4)
+        //{
+            if (Input.mousePosition.x < scrollDistance && transform.position.x > -11)
+                transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime);
+            else if (Input.mousePosition.x >= Screen.width - scrollDistance && transform.position.x < 15)
+                transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
 
         // Forward / Backward
-        if (Input.mousePosition.y < scrollDistance)
-            transform.Translate((Vector3.forward - transform.forward) * -scrollSpeed * Time.deltaTime);
-        else if (Input.mousePosition.y >= Screen.height - scrollDistance)
-            transform.Translate((Vector3.forward - transform.forward) * scrollSpeed * Time.deltaTime);
-
+            if (Input.mousePosition.y < scrollDistance && transform.position.z < 32)
+                transform.Translate((Vector3.forward - transform.forward) * -scrollSpeed * Time.deltaTime);
+            else if (Input.mousePosition.y >= Screen.height - scrollDistance && transform.position.z > -4)
+                transform.Translate((Vector3.forward - transform.forward) * scrollSpeed * Time.deltaTime);
+        //}
         // Zooming
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
             transform.Translate((Vector3.forward + transform.forward) * -scrollSpeed * Time.deltaTime);
