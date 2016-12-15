9cf46eaf96c5a3dad49f255728877a9211382292
Logan Douglass <lcdouglass5352@pointloma.edu>
2016-12-15 10:05:29 -0800

    Changed main menu's "Load Game" so that if there is no save file, it loads up Level 1.

--- ProjectStrategy/Assets/Scripts/newbutton.cs
+++ ProjectStrategy/Assets/Scripts/newbutton.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using UnityEngine.SceneManagement;
 
 public class newbutton : MonoBehaviour {
 
 	public void loadGame1()
     {
-       // saver mySaver = this.gameObject.AddComponent<saver>();
-        saver.loadGame();
+        //saver mySaver = this.gameObject.AddComponent<saver>();
+        if(File.Exists(Application.persistentDataPath + "/strategygame.save"))
+        {
+            saver.loadGame();
+        }
+        else
+        {
+            SceneManager.LoadScene("Main");
+        }
     }
 }
